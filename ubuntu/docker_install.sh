6a277d5e6763142f96ae1651af057f68267fc71e
Trev Harmon <trevharmon@gmail.com>
2022-02-19 19:06:28 -0700

    add docker install file
    This is for bringing Docker online.

---
+++ ubuntu/docker_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo '### Install Docker ###'
+sudo echo
+echo '--- Install System Packages ---'
+sudo apt update
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    ;
+
+echo
+echo '--- Add Docker Repository ---'
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo
+echo '--- Install Docker Packages ---'
+sudo apt update
+sudo apt install \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    ;
+
+echo
+echo '--- Start Docker ---'
+sudo usermod -a -G docker $USER
+sudo systemctl enable docker
+sudo systemctl start docker
+
+echo
+echo '--- Validate Install ---'
+sudo docker run hello-world
+
+echo
+echo '--- Clean Up ---'
+sudo docker rm $(sudo docker ps -a | grep 'hello-world' | sed 's/ .*//')
+sudo docker rmi 'hello-world'
