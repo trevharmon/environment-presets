81ef3f216c7e631abd1e55f1eb8454ee466b4512
Trev Harmon <trevharmon@gmail.com>
2022-08-30 00:05:31 -0600

    add `git` general environment configuration
    This provides a basic setup for the `git` environment.

---
+++ git/configure_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "Trev Harmon"
+git config --global user.email trevharmon@gmail.com
+git config --global core.editor vim
+git config --global color.ui true
+git config --global init.defaultBranch main
